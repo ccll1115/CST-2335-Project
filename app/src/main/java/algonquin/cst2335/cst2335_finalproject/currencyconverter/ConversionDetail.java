4bfb1b99439098c9678366264ecf55908b3b9ad1
Eva Wang <wang0900@algonquinlive.com>
2023-07-20 20:45:45 -0400

    Currency Converter: week 1 - final

--- app/src/main/java/algonquin/cst2335/cst2335_finalproject/currencyconverter/ConversionDetail.java
+++
@@ -1,6 +0,0 @@
-package algonquin.cst2335.cst2335_finalproject.currencyconverter;
-
-
-public class ConversionDetail {
-
-}
--- app/src/main/java/algonquin/cst2335/cst2335_finalproject/currencyconverter/CurrencyConverter.java
+++ app/src/main/java/algonquin/cst2335/cst2335_finalproject/currencyconverter/CurrencyConverter.java
@@ -1,27 +1,38 @@
 package algonquin.cst2335.cst2335_finalproject.currencyconverter;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.material.snackbar.Snackbar;
 
 import org.w3c.dom.Text;
 
 import java.util.ArrayList;
 
+import algonquin.cst2335.cst2335_finalproject.MainActivity;
 import algonquin.cst2335.cst2335_finalproject.R;
+import algonquin.cst2335.cst2335_finalproject.bearimage.BearImage;
 import algonquin.cst2335.cst2335_finalproject.databinding.ActivityCurrencyConverterBinding;
 import algonquin.cst2335.cst2335_finalproject.databinding.CurrencyConversionDetailBinding;
+import algonquin.cst2335.cst2335_finalproject.flighttracker.FlightTracker;
+import algonquin.cst2335.cst2335_finalproject.triviaquestion.TriviaQuestion;
 
 public class CurrencyConverter extends AppCompatActivity {
     ActivityCurrencyConverterBinding binding;
@@ -33,6 +44,43 @@ public class CurrencyConverter extends AppCompatActivity {
     ArrayList<String> conversionLog = new ArrayList<>();
 
     @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+
+        getMenuInflater().inflate(R.menu.my_menu, menu);
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        // intent to each activity page
+        Intent first = new Intent(CurrencyConverter.this, FlightTracker.class);
+        Intent third = new Intent(CurrencyConverter.this, TriviaQuestion.class);
+        Intent fourth = new Intent(CurrencyConverter.this, BearImage.class);
+        Intent main = new Intent(CurrencyConverter.this, MainActivity.class);
+        // intent actions
+       if (item.getItemId() == R.id.icon_flight) {
+            startActivity(first);
+        } else if (item.getItemId() == R.id.icon_question) {
+            startActivity(third);
+        } else if (item.getItemId() == R.id.icon_bear) {
+            startActivity(fourth);
+        } else
+        if (item.getItemId() == R.id.icon_back) {
+            startActivity(main);
+        } else if (item.getItemId() == R.id.icon_help) {
+            AlertDialog.Builder builder = new AlertDialog.Builder(CurrencyConverter.this);
+            builder.setTitle("HELP: ")
+                    .setMessage("- Input a number\n- Pick a currency (from)\n- Pick a currency (to)\n" +
+                            "- Click CONVERT to make a conversion\n- Click ARROWS between currencies to swap them")
+                    .setPositiveButton("OK", (dialog, click) -> {}).create().show();
+        }
+        return super.onOptionsItemSelected(item);
+
+    }
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
@@ -40,11 +88,17 @@ public class CurrencyConverter extends AppCompatActivity {
         setContentView(binding.getRoot());
         //setContentView(R.layout.activity_currency_converter);
 
+        setSupportActionBar(binding.myToolbar);
+
         loadFromPrefs();
+
+
 
         binding.btnSwitch.setOnClickListener(clk -> {
             String strForm = binding.editFromCurrency.getText().toString();
             String strTo = binding.editToCurrency.getText().toString();
+
+            Toast.makeText(this, "You switched currency conversion order.", Toast.LENGTH_LONG).show();
 
             binding.editFromCurrency.setText(strTo);
             binding.editToCurrency.setText(strForm);
@@ -57,6 +111,7 @@ public class CurrencyConverter extends AppCompatActivity {
             String strNewAmount = binding.editNewAmount.getText().toString();
 
             String strLog = strAmount + " " + strForm + " = " + strNewAmount + " " + strTo;
+            Snackbar.make(this.getCurrentFocus(), "Conversion calculating for you.", Snackbar.LENGTH_LONG).show();
 
             conversionLog.add(strLog);
             myAdapter.notifyDataSetChanged();
@@ -150,6 +205,13 @@ public class CurrencyConverter extends AppCompatActivity {
 
             textConverDetail = itemView.findViewById(R.id.txtConverDetail);
             textConverTime = itemView.findViewById(R.id.txtConverTime);
+
+            itemView.setOnClickListener( click ->{
+                
+
+
+
+            });
         }
     }
 }
